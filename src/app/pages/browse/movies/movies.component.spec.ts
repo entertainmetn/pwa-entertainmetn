0ad82b19529a0e6775248edd98711a0f20d078d3
Seifallah GUEDOUARI <seifallah.guedouari@sofrecom.com>
2017-07-04 15:59:57 +0100

    unit test works

--- src/app/pages/browse/movies/movies.component.spec.ts
+++ src/app/pages/browse/movies/movies.component.spec.ts
@@ -1,27 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Observable } from 'rxjs/Observable'
-import { ActivatedRoute, Data } from '@angular/router';
+import {Params, ActivatedRoute} from '@angular/router';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 import { MoviesComponent } from './movies.component';
+import { Injectable } from '@angular/core';
 
 // ADDED CLASS
-class MockRouter {
-    navigateByUrl(url: string) { return url; }
+@Injectable()
+export class ActivatedRouteStub {
+    private subject = new BehaviorSubject(this.testParams);
+    params = this.subject.asObservable();
+
+    private _testParams: {};
+    get testParams() { return this._testParams; }
+    set testParams(params: {}) {
+        this._testParams = params;
+        this.subject.next(params);
+    }
 }
 
 describe('MoviesComponent', () => {
+  let mockActivatedRoute;
   let component: MoviesComponent;
   let fixture: ComponentFixture<MoviesComponent>;
 
   beforeEach(async(() => {
+    mockActivatedRoute = new ActivatedRouteStub();
+    mockActivatedRoute.testParams = { id: Observable.of ('ddfsdfsdfzsd')};
     TestBed.configureTestingModule({
       providers: [
-        {
-          provide: ActivatedRoute,
-          useValue: {
-            data: Observable.of({ id: 'yolo' })
-          }
-        }
+          { provide: ActivatedRoute, useValue: mockActivatedRoute }
       ],
       declarations: [MoviesComponent]
     })
