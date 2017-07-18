e6794812663d500e6e7366400ca4949fceee5bf4
Seifallah GUEDOUARI <seifallah.guedouari@sofrecom.com>
2017-07-18 18:24:09 +0100

    query params in browse

--- src/app/pages/browse/browse.component.ts
+++ src/app/pages/browse/browse.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'xstr-browse',
@@ -6,8 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./browse.component.css']
 })
 export class BrowseComponent implements OnInit {
+  paramString: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+
+    this.paramString = 'test params ='
+    this.route.queryParamMap.forEach(
+      params => {
+        console.log('queryparam changed')
+        params.keys.forEach(paramkey => {
+          this.paramString = this.paramString + ' ' + paramkey + ' = ' + params.getAll(paramkey);
+          console.log('param found => ' + paramkey + ' = ' + params.get(paramkey));
+        });
+
+      }
+    );
+  }
 
   ngOnInit() {
   }
