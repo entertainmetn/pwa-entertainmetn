6e24033ea28b9093fd3cb6cb422d4881df9ed619
seifallah Guedouari <guedouari@gmail.com>
2017-08-14 22:04:59 +0100

    sideNav improvement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,19 +10,29 @@ export class AppComponent {
   title = 'xstr';
   pixelWidth: number;
   pixelHeight: number;
+  sideNavOpened = true;
 
   constructor() {
     this.pixelWidth = document.defaultView.innerWidth;
     this.pixelHeight = document.defaultView.innerHeight;
     console.log('size intialized');
+    this.sideNavDynamic();
   }
 
   onResize(event) {
-    console.log(event);
-    console.log( 'width:' + event.target.innerWidth);
-    console.log( 'height:' + event.target.innerHeight);
+    // console.log(event);
+    // console.log('width:' + event.target.innerWidth);
+    // console.log('height:' + event.target.innerHeight);
 
     this.pixelWidth = event.target.innerWidth;
     this.pixelHeight = event.target.innerHeight;
+    this.sideNavDynamic();
+  }
+  sideNavDynamic() {
+    if (this.pixelWidth > 600) {
+      this.sideNavOpened = true;
+    } else {
+      this.sideNavOpened = false;
+    }
   }
 }
