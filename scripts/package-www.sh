22cb509c7fe1a9984a4fe3b6f740b0aa8ba87cc7
seifallah Guedouari <guedouari@gmail.com>
2017-09-12 01:56:07 +0100

    restart tar script

--- scripts/package-www.sh
+++ scripts/package-www.sh
@@ -6,7 +6,4 @@ abort() {
 }
 [ -z $TRAVIS_TAG ] && abort "tag-undefined"
 echo "creating archive for ""$TRAVIS_TAG"
-echo "content of www :"
-echo "build dir => " $TRAVIS_BUILD_DIR
-echo $(ls -l $TRAVIS_BUILD_DIR/www)
-tar -zcvf $TRAVIS_BUILD_DIR"/www/client.xstr.me-"$TRAVIS_TAG".tar.gz" $TRAVIS_BUILD_DIR"/www"
+tar -zcvf www/client.xstr.me-$TRAVIS_TAG.tar.gz www
