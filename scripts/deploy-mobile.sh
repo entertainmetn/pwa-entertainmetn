aa0a680aa534151ede79e47845ff685aa9e195bf
seifallah Guedouari <guedouari@gmail.com>
2017-09-15 16:42:22 +0100

    add deply to mobile.xstr.me

---
+++ scripts/deploy-mobile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+git clone --depth 1 https://travis-ci:${GITHUB_TOKEN}@github.com/guedouari/mobile.xstr.me.git
+cd mobile.xstr.me/
+# git remote rm origin
+# git remote add origin https://travis-ci:${GITHUB_TOKEN}@github.com/guedouari/desktop.xstr.me.git
+
+
+
+cp ../client.xstr.me-$TRAVIS_TAG.tar.gz www/www.tar.gz
+
+
+
+git add www/www.tar.gz
+
+git commit -m "update package from xstr.me via script "$TRAVIS_TAG
+
+git tag $TRAVIS_TAG
+
+git push
+
+git push --tags
+
