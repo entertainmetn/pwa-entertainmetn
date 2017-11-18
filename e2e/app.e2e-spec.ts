607cfc9b0d1f6184f84b1064b06e19dd46e499c4
seifallah Guedouari <guedouari@gmail.com>
2017-11-18 22:19:20 +0100

    fix e2e tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,15 +7,15 @@ describe('xstr.me App', () => {
     page = new XstrMePage();
   });
 
-  /* it('should display welcome message', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('home works!');
-  }); */
-
-  it('should display welcome message', (done) => {
-    page.navigateTo();
-    page.getParagraphText()
-      .then((msg) => expect(msg).toEqual('home works!'))
-      .then(done, done.fail);
+/* keep old promise methode just in case
+    it('should display welcome message', (done) => {
+      page.navigateTo();
+      page.getParagraphText()
+        .then((msg) => expect(msg).toEqual('home works!'))
+        .then(done, done.fail);
+        */
   });
 });
